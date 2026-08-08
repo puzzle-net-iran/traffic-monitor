ef1b62adabe0348723d3f811a7403c01c8bbe431
گروه فنی پازل نت ایران/puzzle net group iran <161571468+puzzle-net-iran@users.noreply.github.com>
2026-08-08 17:13:31 +0330

    Delete app directory

--- app/src/main/java/ir/trafficmonitor/app/App.kt
+++
@@ -1,8 +0,0 @@
-package ir.trafficmonitor.app
-
-import android.app.Application
-import ir.trafficmonitor.app.db.AppDatabase
-
-class App : Application() {
-    val database: AppDatabase by lazy { AppDatabase(this) }
-}
